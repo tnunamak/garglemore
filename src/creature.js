0ce1fb2dc5a757dd89b8de5963efc83d16c155e1
Brennan N <brennan@toshop.com>
2018-10-06 20:55:08 +0200

    Initial enemy creation (#12)
    * Initial enemy creation
    
    Details:
    - Enemies are spawned at random coordinates
    - Enemies are scaled according to their stats
    
    * Creature Improvements
    
    - Creatures collide with walls and the player
    - Creatures move towards the player at a pixel speed weighted by their
    stats
    - Changed creature sprite (no animations yet)
    - Deleted duplicate top-level 'assets' folder
    
    * Creature Animations
    
    - Updated creature sprites (zombie)
    - Added creature movement animations (creation, play)

---
+++ src/creature.js
@@ -0,0 +1,80 @@
+export default class Creature{
+
+	constructor(scene, x, y, stats){
+		this.scene 	= scene;
+		this.sprite = scene.physics.add.sprite(x, y, 'zombie');
+		this.stats 	= stats;
+		this.scale 	= this.getScaleFromStats(stats);
+		this.sprite.setScale(this.scale);
+	}
+
+	getScaleFromStats(statVals){	
+		let statSum = 0.0;
+
+		for(let statName in statVals){
+			let statVal = statVals[statName]
+			if(statName == 'speed')
+				statVal = 1 - statVals[statName];
+
+			statSum = statSum + statVal;
+		}
+
+		let scale = statSum / Object.keys(statVals).length;
+		return scale + 1;
+	}
+
+	moveTowards(renderObj){
+		let pxSpeed = this.getSpeedInPx();
+		this.scene.physics.moveToObject(this.sprite, renderObj, pxSpeed);
+		this.animateMovement(renderObj);
+	}
+
+	animateMovement(renderObj){
+		let direction = this.getMovementDirection(renderObj);
+      	this.sprite.anims.play('zombie-' + direction, true);
+	}
+
+	getMovementDirection(renderObj){
+		let deltaX = renderObj.x - this.sprite.x;
+		let deltaY = renderObj.y - this.sprite.y;
+		
+		if(Math.abs(deltaX) <= Math.abs(deltaY) && deltaY < 0){
+			return 'up';
+		}
+
+		if(Math.abs(deltaX) <= Math.abs(deltaY) && deltaY > 0){
+			return 'down';
+		}
+
+		if (Math.abs(deltaY) <= Math.abs(deltaX) && deltaX > 0) {
+			return 'right';
+		}
+
+		if (Math.abs(deltaY) <= Math.abs(deltaX) && deltaX < 0) {
+			return 'left';
+		}
+
+		return 'down';
+	}
+
+	getSpeedInPx(){
+		const SPEED_UNIT_PX 	= 350;
+
+		return this.getSpeed() * SPEED_UNIT_PX;
+	}
+
+	getSpeed(){
+		const MAX_SPEED_PCT 	= 0.6; 
+		const MIN_SPEED_PCT 	= 0.2; 
+
+		if (this.stats.speed > MAX_SPEED_PCT) {
+			return MAX_SPEED_PCT;
+		}
+
+		if (this.stats.speed < MIN_SPEED_PCT) {
+			return MIN_SPEED_PCT;
+		}
+
+		return this.stats.speed;
+	}
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import 'phaser';
 
 import archetypes from './archetypes'
 import getStats from './stats'
+import Creature         from './creature';
 import Cursors from './player/player-movement.js'
+import DynamicGroup    from './dynamicGroup';
 
 function makeCharacter(level, type) {
     getStats(level, archetypes[type].modifiers)
@@ -32,6 +34,7 @@ var config = {
 let player;
 let stars;
 let platforms;
+var creatureGroup;
 let horizontalWalls;
 let verticalWalls;
 
@@ -42,6 +45,7 @@ function preload() {
     this.load.image('horizontal_wall', 'public/assets/images/basic-wall-30x60.png')
     this.load.image('vertical_wall', 'public/assets/images/vertical-wall-60x30.png')
     this.load.spritesheet('dude', 'public/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('zombie', 'public/assets/zombie.png', { frameWidth: 32, frameHeight: 42 });
 }
 
 function joinPlayer (pad) {
@@ -53,8 +57,10 @@ function joinPlayer (pad) {
   // physics interactions
   this.physics.add.collider(player, horizontalWalls);
   this.physics.add.collider(player, verticalWalls);
+  creatureGroup.collidesWith(player);
 
   this.physics.add.overlap(player, stars, collectStar, null, this);
+
 
   return {
     player,
@@ -64,55 +70,101 @@ function joinPlayer (pad) {
 
 const players = new Map()
 
-function create () {
-  //Create all the things
-  horizontalWalls = this.physics.add.staticGroup();
-  verticalWalls = this.physics.add.staticGroup();
-  this.input.gamepad.on('down', function (pad, button, index) {
-    if (!players.has(pad)) {
-      players.set(pad, joinPlayer.bind(this)(pad))
+function create() {
+    //Create all the things
+    horizontalWalls = this.physics.add.staticGroup();
+    verticalWalls = this.physics.add.staticGroup();
+    this.input.gamepad.on('down', function (pad, button, index) {
+        if (!players.has(pad)) {
+          players.set(pad, joinPlayer.bind(this)(pad))
+        }
+      }, this)
+
+    for (let i = 1; i <= 20; i++) {
+        horizontalWalls.create(i * 60 - 30, 0, 'horizontal_wall');
+        horizontalWalls.create(i * 60 - 30, 780, 'horizontal_wall');
+        verticalWalls.create(0, i * 60 - 30, 'vertical_wall');
+        verticalWalls.create(1200, i * 60 - 30, 'vertical_wall');
     }
-  }, this)
+   
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
-  for (let i = 1; i <= 20; i++) {
-    horizontalWalls.create(i * 60 - 30, 0, 'horizontal_wall');
-    horizontalWalls.create(i * 60 - 30, 780, 'horizontal_wall');
-    verticalWalls.create(0, i * 60 - 30, 'vertical_wall');
-    verticalWalls.create(1200, i * 60 - 30, 'vertical_wall');
-  }
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
 
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-  });
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
-  this.anims.create({
-    key: 'turn',
-    frames: [{ key: 'dude', frame: 4 }],
-    frameRate: 20
-  });
+    // stars
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
 
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-  });
+    stars.children.iterate(function (child) {
 
-  // stars
-  stars = this.physics.add.group({
-    key: 'star',
-    repeat: 11,
-    setXY: { x: 12, y: 0, stepX: 70 }
-  });
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
-  this.physics.add.collider(stars, horizontalWalls);
+    });
+    this.anims.create({
+        key: 'zombie-down',
+        frames: this.anims.generateFrameNumbers('zombie', { start: 0, end: 2 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
-  stars.children.iterate(function (child) {
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
+    this.anims.create({
+        key: 'zombie-left',
+        frames: this.anims.generateFrameNumbers('zombie', { start: 3, end: 5 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'zombie-right',
+        frames: this.anims.generateFrameNumbers('zombie', { start: 6, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'zombie-up',
+        frames: this.anims.generateFrameNumbers('zombie', { start: 9, end: 11 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    let creatures = [];
+
+    for (var i = 0; i < 10; i++) {
+        const x = Phaser.Math.Between(50, 1150);
+        const y = Phaser.Math.Between(50, 730);
+
+        let stats = {
+          health: Phaser.Math.Between(1, 100) / 100,
+          speed: Phaser.Math.Between(1, 100) / 100,
+          attack: Phaser.Math.Between(1, 100) / 100
+        }
+
+        creatures.push(new Creature(this, x, y, stats));
+    }
+
+    creatureGroup = new DynamicGroup(this, creatures);
+    creatureGroup.collidesWith(horizontalWalls);
+    creatureGroup.collidesWith(verticalWalls);
 }
 
 function update() {
@@ -124,7 +176,7 @@ function update() {
 
 function updatePlayer ({ player, movement }, gamepad) {
   movement.checkMovement(gamepad);
-
+  creatureGroup.moveTowards(player);
   //if (gamepad.A && player.body.velocity.y >= 0) {
     //player.setVelocityY(-330);
   //}
