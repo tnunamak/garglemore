c6b1fe1aee7f7d0acb7322a2bb0ea944e00e369b
Tim Nunamaker <tim.nunamaker@bazaarvoice.com>
2018-10-05 22:20:51 -0500

    Generate stats for a character

---
+++ public/js/archetypes.js
@@ -0,0 +1,33 @@
+// Modifiers are small numbers that represent
+// how much more of an attribute this type of
+// character has than others.
+module.exports = {
+  shooter: {
+    modifiers: {
+      health: 0.03,
+      speed: 0.06,
+      attack: 0.005
+    }
+  },
+  dasher: {
+    modifiers: {
+      health: 0.03,
+      speed: 0.06,
+      attack: 0.005
+    }
+  },
+  grenadier: {
+    modifiers: {
+      health: 0.03,
+      speed: 0.06,
+      attack: 0.005
+    }
+  },
+  kamikaze: {
+    modifiers: {
+      health: 0.03,
+      speed: 0.06,
+      attack: 0.005
+    }
+  }
+}
